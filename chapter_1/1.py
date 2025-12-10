023a6eb45eafc4a7e10249d4d612ce8770a4f3c4
xthan <hackattendant@yahoo.com>
2025-12-10 12:53:56 -0500

    add notes up to 1.1.2

--- chapter_1/1.py
+++ chapter_1/1.py
@@ -6,3 +6,19 @@ lst = [1, 2, 3, 4, 5, 6]
 arr = np.array(lst)
 print(arr)
 print()
+# creating an array of floats from list
+arr = np.array(lst, dtype=float)
+print(arr)
+print()
+
+# create multi dimensional array from nested list
+names = [["Jon", "Mary", "Paul"], ["Peter", "Ben", "Saul"]]
+arr = np.array(names)
+print(arr)
+print()
+print("array dimensions")
+print(f"length: {arr.shape}")
+print(f"nDimensions: {arr.ndim}")
+
+
+print("_____________1.1.2______________________")

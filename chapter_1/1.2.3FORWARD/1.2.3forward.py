e21f9f83c1f2a874217e2de2fc9e9796974eef28
xthan <hackattendant@yahoo.com>
2025-12-12 10:33:20 -0600

    add filter array 1.2.3

--- chapter_1/1.2.3FORWARD/1.2.3forward.py
+++ chapter_1/1.2.3FORWARD/1.2.3forward.py
@@ -1,4 +1,12 @@
 import numpy as np
 
 print("_____________1.2.3______________________")
-
+# Boolean Indexing
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# create a boolean mask based on the condition
+filter_array = arr % 2 != 0
+print(arr)
+print(filter_array)
+# select the elements from the original array based on the filter
+selected = arr[filter_array]
+print(selected)
